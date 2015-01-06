5b94f3fd796b5b0f2728148a78a88de2dfd01a98
Jonathan L Long <jonlong@cs.berkeley.edu>
2015-01-05 21:16:51 -0800

    [pycaffe] re-expose Layer

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -1,6 +1,6 @@
 from .pycaffe import Net, SGDSolver
 from ._caffe import set_mode_cpu, set_mode_gpu, set_phase_train, \
-        set_phase_test, set_device
+        set_phase_test, set_device, Layer
 from .classifier import Classifier
 from .detector import Detector
 import io
