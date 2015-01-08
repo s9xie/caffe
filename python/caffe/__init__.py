54c5e9bae17c17daf3e8062fcb04497063f56fb3
Jonathan L Long <jonlong@cs.berkeley.edu>
2015-01-07 16:47:52 -0800

    [pycaffe] expose global ("Caffe::") functions

--- python/caffe/__init__.py
+++ python/caffe/__init__.py
@@ -1,4 +1,6 @@
 from .pycaffe import Net, SGDSolver
+from ._caffe import set_mode_cpu, set_mode_gpu, set_phase_train, \
+        set_phase_test, set_device
 from .classifier import Classifier
 from .detector import Detector
 import io
